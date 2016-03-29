f30e72b6743df89a18a2f387b793183c30ce1c85
Frank Rittners <frank@rittners.de>
2016-03-29 22:10:14 +0200

    Initial Commit

---
+++ fr_PageBased/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fr_PageBased
+//
+//  Created by Frank Rittners on 29.03.16.
+//  Copyright © 2016 Frank Rittners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
